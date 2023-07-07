ed22434766594d0af5c5721e2db361febacabe20
charlenefialho <fialhocharlene@gmail.com>
2023-07-07 09:54:22 -0300

    refactor: changed i to token

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,32 @@
-import { Token } from './interfaces';
-import { TokenMarket } from './interfaces';
+import { Token, TokenMarket } from "./interfaces";
 
 export class User {
-  id: number
+  id: number;
   nameUser: string;
   balance: number;
   tokens: Token[];
 
-  constructor(id: number,nameUser: string,balance:number,tokens:Token[]){
-      this.id = id;
-      this.nameUser = nameUser;
-      this.balance = balance;
-      this.tokens = tokens;
+  constructor(id: number, nameUser: string, balance: number, tokens: Token[]) {
+    this.id = id;
+    this.nameUser = nameUser;
+    this.balance = balance;
+    this.tokens = tokens;
   }
 }
 
-
-export function showTransactionReport(user: User , purchasedTokens : TokenMarket){
+export function showTransactionReport(
+  user: User,
+  purchasedTokens: TokenMarket
+) {
   alert(`Relatório de transação:\n 
   Nome do usuário: ${user.nameUser}\n
   Saldo restante: R$${user.balance.toFixed(2)}\n
-  `)
-  
+  `);
+
   purchasedTokens.tokens.forEach((token) => {
     alert(`Tokens comprados:  \n
-    ${token.nameToken}: quantidade: ${purchasedTokens.tokens.length}, \nValor R$${token.value.toFixed(2)}`);
+    ${token.nameToken}: quantidade: ${
+      purchasedTokens.tokens.length
+    }, \nValor R$${token.value.toFixed(2)}`);
   });
 }
-
-
--- src/purchase.ts
+++ src/purchase.ts
@@ -1,12 +1,12 @@
-import { User } from './app';
-import { Token } from './interfaces';
-import { TokenMarket } from './interfaces';
-const divTokenList = document.querySelector('#tokenCards') as HTMLElement;
+import { User } from "./app";
+import { Token, TokenMarket } from "./interfaces";
 
-document.addEventListener('DOMContentLoaded', () => {
+const divTokenList = document.querySelector("#tokenCards") as HTMLElement;
+
+document.addEventListener("DOMContentLoaded", () => {
   getTokenList();
   showAvailableTokens();
-  setIntervalAddNewToken(); 
+  setIntervalAddNewToken();
 });
 
 setIntervalAddNewToken();
@@ -21,83 +21,83 @@ export function createToken(): Token {
   return token;
 }
 
-export async function AddNewToken(){
+export async function AddNewToken() {
   const token = createToken();
 
-  await fetch('http://localhost:3000/tokens',{
-    method: 'POST',
+  await fetch("http://localhost:3000/tokens", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(token)
+    body: JSON.stringify(token),
   })
-  .then(response => response.json())
-  .then(result => {
-    console.log('Dados enviados com sucesso:', result);
-  })
-  .catch(error => {
-    console.error('Erro ao enviar os dados:', error);
-  });
-
-  
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("Dados enviados com sucesso:", result);
+    })
+    .catch((error) => {
+      console.error("Erro ao enviar os dados:", error);
+    });
 }
 
-async function setIntervalAddNewToken(){
+async function setIntervalAddNewToken() {
   await setInterval(AddNewToken, 60000);
 }
 
 export async function getTokenList() {
   try {
-    const response = await fetch('http://localhost:3000/tokens');
+    const response = await fetch("http://localhost:3000/tokens");
 
     if (!response.ok) {
       throw new Error("Ocorreu um erro na requisição GET.");
     }
     const data = await response.json();
     return data;
-
   } catch (error) {
     alert(error);
-    throw error; 
+    throw error;
   }
 }
 
-export async function showAvailableTokens(){
-    await getTokenList().then((tokenList: Token[])=>{
-      for (let i in tokenList) {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.innerHTML = `
-          <h2>${tokenList[i].nameToken}</h2>
-          <p>Valor: R$${tokenList[i].value.toFixed(2)}</p>
-          <p>Quantidade disponível: ${tokenList[i].quantity}</p>
-          <button class="buyButton" id="${tokenList[i].id}">Comprar</button>
+export async function showAvailableTokens() {
+  await getTokenList().then((tokenList: Token[]) => {
+    for (let token in tokenList) {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      card.innerHTML = `
+          <h2>${tokenList[token].nameToken}</h2>
+          <p>Valor: R$${tokenList[token].value.toFixed(2)}</p>
+          <p>Quantidade disponível: ${tokenList[token].quantity}</p>
+          <button class="buyButton" id="${tokenList[token].id}">Comprar</button>
         `;
-        divTokenList.appendChild(card);
-  
-        const buyButton = card.querySelector('.buyButton') as HTMLButtonElement;
-        buyButton.addEventListener('click', buyToken)
-      }
-    })
+      divTokenList.appendChild(card);
+
+      const buyButton = card.querySelector(".buyButton") as HTMLButtonElement;
+      buyButton.addEventListener("click", buyToken);
+    }
+  });
 }
 
- 
-function checkTokenAmount(amountOfTokens: string |null, user: User, token: Token) {
-  if (amountOfTokens === null || amountOfTokens === '0') {
+function checkTokenAmount(
+  amountOfTokens: string | null,
+  user: User,
+  token: Token
+) {
+  if (amountOfTokens === null || amountOfTokens === "0") {
     cancelPurchase();
   } else if (!isNaN(parseInt(amountOfTokens))) {
     const numberOfTokens = parseInt(amountOfTokens);
     handleTokenAmount(numberOfTokens, user, token);
-  }else{
-    alert('A quantidade de tokens a serem comprados deve ser um número!!')
-  } 
+  } else {
+    alert("A quantidade de tokens a serem comprados deve ser um número!!");
+  }
 }
 
 function cancelPurchase() {
-  alert('Compra cancelada');
+  alert("Compra cancelada");
 }
 
-function handleTokenAmount(numberOfTokens : number, user : User, token : Token) {
+function handleTokenAmount(numberOfTokens: number, user: User, token: Token) {
   if (numberOfTokens === 1) {
     handleSingleTokenPurchase(user, token);
   } else {
@@ -105,7 +105,11 @@ function handleTokenAmount(numberOfTokens : number, user : User, token : Token) 
   }
 }
 
-function handleBatchTokenPurchase(numberOfTokens: number, user: User, token: Token) {
+function handleBatchTokenPurchase(
+  numberOfTokens: number,
+  user: User,
+  token: Token
+) {
   if (numberOfTokens <= 20) {
     buyTokenBatch(user, token, numberOfTokens, 0.2);
   } else if (numberOfTokens >= 21 && numberOfTokens <= 50) {
@@ -117,7 +121,7 @@ function handleBatchTokenPurchase(numberOfTokens: number, user: User, token: Tok
   }
 }
 
-function handleSingleTokenPurchase(user: User, token:Token) {
+function handleSingleTokenPurchase(user: User, token: Token) {
   if (user.balance >= token.value) {
     user.tokens.push(token);
     user.balance -= token.value;
@@ -130,22 +134,20 @@ function handleSingleTokenPurchase(user: User, token:Token) {
 }
 
 function insufficientBalance() {
-  alert('Saldo insuficiente para comprar o token.');
+  alert("Saldo insuficiente para comprar o token.");
 }
-  
-
 
 export async function buyToken(event: Event) {
   const idToken = (event.target as HTMLElement).id;
   const token: Token = await getTokenById(parseInt(idToken));
   const user: User = await getUserById(12);
 
-  const amountOfTokens: string | null = prompt(`Quantos tokens de ${token.nameToken} você quer comprar?`);
+  const amountOfTokens: string | null = prompt(
+    `Quantos tokens de ${token.nameToken} você quer comprar?`
+  );
 
-  checkTokenAmount(amountOfTokens, user,token);
-
+  checkTokenAmount(amountOfTokens, user, token);
 }
-
 
 export async function getUserById(idUser: number) {
   const url = `http://localhost:3001/users/${idUser}`;
@@ -165,7 +167,6 @@ export async function getUserById(idUser: number) {
   }
 }
 
-
 export async function getTokenById(idToken: number) {
   const url = `http://localhost:3000/tokens/${idToken}`;
 
@@ -180,17 +181,20 @@ export async function getTokenById(idToken: number) {
     return data;
   } catch (error) {
     console.error(error);
-    throw error; 
+    throw error;
   }
 }
 
-
-
-export function buyTokenBatch(user: User, token: Token, quantity: number, discount: number) {
+export function buyTokenBatch(
+  user: User,
+  token: Token,
+  quantity: number,
+  discount: number
+) {
   const totalPrice = token.value * quantity * (1 - discount);
   if (user.balance >= totalPrice && token.quantity >= quantity) {
     for (let i = 0; i < quantity; i++) {
-      user.tokens.push(token); 
+      user.tokens.push(token);
       modifyTokenValue(token);
     }
     user.balance -= totalPrice;
@@ -198,59 +202,52 @@ export function buyTokenBatch(user: User, token: Token, quantity: number, discou
     modifyTokenValue(token);
     updateUserTokens(user.id, user);
   } else {
-    alert('Saldo insuficiente ou quantidade de tokens indisponível para comprar em lote.');
+    alert(
+      "Saldo insuficiente ou quantidade de tokens indisponível para comprar em lote."
+    );
   }
-} 
+}
 
 export async function modifyTokenValue(token: Token) {
-  const marketFactor = 1 + 0.02 ; 
+  const marketFactor = 1 + 0.02;
   const newValue = token.value * marketFactor;
   token.value = newValue;
 
   try {
     const url = `http://localhost:3000/tokens/${token.id}`;
     const response = await fetch(url, {
-      method: 'PUT',
+      method: "PUT",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(token)
+      body: JSON.stringify(token),
     });
 
-
     if (!response.ok) {
-      throw new Error('Ocorreu um erro na requisição PUT.');
+      throw new Error("Ocorreu um erro na requisição PUT.");
     }
   } catch (error) {
     alert(error);
     throw error;
   }
-
 }
 
-async function updateUserTokens(id: number, user:User) {
+async function updateUserTokens(id: number, user: User) {
   try {
     const url = `http://localhost:3001/users/${user.id}`;
     const response = await fetch(url, {
-      method: 'PUT',
+      method: "PUT",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(user)
+      body: JSON.stringify(user),
     });
 
     if (!response.ok) {
-      throw new Error('Ocorreu um erro na requisição PUT.');
+      throw new Error("Ocorreu um erro na requisição PUT.");
     }
   } catch (error) {
     console.error(error);
     throw error;
   }
 }
-
-
-
-
-
-
-
